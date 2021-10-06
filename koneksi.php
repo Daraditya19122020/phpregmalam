2f0c35f619f015f4afed5aa59d2c792eb932e775
Daraditya19122020 <daraditya779@gmail.com>
2021-10-06 19:35:46 +0700

    saya sudah selesai

---
+++ koneksi.php
@@ -0,0 +1,14 @@
+<?php
+
+$server="localhost";
+$user="root";
+$password="";
+$database="phpregmalam";
+
+$koneksi=mysqli_connect($server,$user,$password,$database);
+
+if ($koneksi){
+	echo "Koneksi sukses";
+}else{
+	echo "koneksi gagal";
+}
